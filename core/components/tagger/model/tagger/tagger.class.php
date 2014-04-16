b594eeccdbff431ffc8306dae831f7f54fdbf978
Jan Peca <pecajan@gmail.com>
2014-04-16 13:09:57 +0200

    Duplicate Tags also for children. Resolves #11

--- core/components/tagger/model/tagger/tagger.class.php
+++ core/components/tagger/model/tagger/tagger.class.php
@@ -244,5 +244,29 @@ class Tagger {
             $newRelation->set('tag', $oldRelation->tag);
             $newRelation->save();
         }
+
+        $this->duplicateChildren($oldResource->Children, $newResource->Children);
+    }
+
+    private function duplicateChildren($oldChildren, $newChildren) {
+        if (empty($oldChildren) || empty($newChildren)) return;
+
+        if (count($oldChildren) != count($newChildren)) return;
+
+        $oldNew = array_combine(array_keys($oldChildren), array_keys($newChildren));
+
+        foreach ($oldChildren as $key => $oldChild) {
+            $oldRelations = $this->modx->getIterator('TaggerTagResource', array('resource' => $oldChild->id));
+
+            /** @var TaggerTagResource $oldRelation */
+            foreach ($oldRelations as $oldRelation) {
+                $newRelation = $this->modx->newObject('TaggerTagResource');
+                $newRelation->set('resource', $newChildren[$oldNew[$key]]->id);
+                $newRelation->set('tag', $oldRelation->tag);
+                $newRelation->save();
+            }
+
+            $this->duplicateChildren($oldChild->Children, $newChildren[$oldNew[$key]]->Children);
+        }
     }
 }
